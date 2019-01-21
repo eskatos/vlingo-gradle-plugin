2d423604fd155871ed72b701a6fcf8d367b90623
Paul Merlin <paul@nosphere.org>
2019-01-21 12:10:50 +0100

    It is compatible with 4.7, add that version to the test matrix

--- src/test/kotlin/io/vlingo/gradle/TestEnvironment.kt
+++ src/test/kotlin/io/vlingo/gradle/TestEnvironment.kt
@@ -8,12 +8,13 @@ val quickTest: Boolean
 val supportedGradleVersions =
         if (quickTest) listOf(
                 "5.1.1",
-                "4.8"
+                "4.7"
         )
         else listOf(
                 "5.1.1",
                 "5.0",
                 "4.10",
                 "4.9",
-                "4.8"
+                "4.8",
+                "4.7"
         )
